338084588917e87db0cf43faddae20266df42c03
Toni Ruottu <toni.ruottu@maas.global>
2019-08-16 17:07:09 +0300

    Clean up type signatures

--- src/scrapql.ts
+++ src/scrapql.ts
@@ -1,7 +1,6 @@
 import { array } from 'fp-ts/lib/Array';
 import * as Array_ from 'fp-ts/lib/Array';
 import * as Foldable_ from 'fp-ts/lib/Foldable';
-import * as Apply_ from 'fp-ts/lib/Apply';
 import * as Record_ from 'fp-ts/lib/Record';
 import { Task, task, taskSeq } from 'fp-ts/lib/Task';
 import * as Task_ from 'fp-ts/lib/Task';
@@ -10,197 +9,172 @@ import * as Option_ from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { identity } from 'fp-ts/lib/function';
 
+// all processors share these generic processor types
+
+export type ResultProcessor<A, R> = (a: A, r: R, ...c: Array<string>) => Task<void>;
+export type QueryProcessor<A, Q, R> = (a: A, q: Q, ...c: Array<string>) => Task<R>;
+
 // fields result contains part of the payload
 
-type FieldsReporter<D, R, K extends Array<string>> = (d: D, r: R, ...k: K) => Task<void>;
-export type FieldsResultProcessor<D, R, K extends Array<string>> = (d: D, r: R, ...k: K) => Task<void>;
+type FieldsReporterConnector<A, R> = (a: A) => (r: R, ...c: Array<string>) => Task<void>;
 
-function processResultFields<D, R, K extends Array<string>>(
-  reporter: FieldsReporter<D, R, K>,
-): FieldsResultProcessor<D, R, K> {
-  return (driver, result, ...keys) => reporter(driver, result, ...keys);
+export function processResultFields<A, R>(connect: FieldsReporterConnector<A, R>): ResultProcessor<A, R> {
+  return (reporters, result, ...context) => connect(reporters)(result, ...context);
 }
 
-// keys result contains data that always exists in data source
+// keys result contains data that always exists in database
 
-type KeysReporter<D, R, K extends Array<string>> = (d: D, r: R, i: string, ...k: K) => Task<void>;
-export type KeysResultProcessor<D, R, K extends Array<string>> = (d: D, r: R, ...k: K) => Task<void>;
+type SubResultProcessor<A, R extends Record<string, any>> = <I extends keyof R>(
+  a: A,
+  r: R[I],
+  i: I,
+  ...k: Array<string>
+) => Task<void>;
 
-function processResultKeys<D, V, K extends Array<string>>(
-  reporter: KeysReporter<D, V, K>,
-): KeysResultProcessor<D, Record<string, V>, K> {
-  return (driver: D, result: Record<string, V>, ...keys: K) => {
-    const entries = Object.entries(result);
-    const reporters: Array<Task<void>> = entries.map(([id, subResult]) => reporter(driver, subResult, id, ...keys));
-    return Foldable_.traverse_(taskSeq, array)(reporters, identity);
+export function processResultKeys<A, R extends Record<string, any>>(
+  subProcessor: SubResultProcessor<A, R>,
+): ResultProcessor<A, R> {
+  return (reporters: A, result: R, ...context: Array<string>) => {
+    const tasks: Array<Task<void>> = pipe(
+      result,
+      Record_.mapWithIndex((key, subResult) => subProcessor(reporters, subResult, key, ...context)),
+      Record_.toUnfoldable(array),
+      Array_.map(([k, v]) => v),
+    );
+    return Foldable_.traverse_(taskSeq, array)(tasks, identity);
   };
 }
 
-// ids result contains data that may not exist in data source
+// ids result contains data that may not exist in database
 
-type IdsReporter<D, R, K extends Array<string>> = KeysReporter<D, R, K>;
-type ExistenceReporter<D> = (d: D, i: string, b: boolean) => Task<void>;
-export type IdsResultProcessor<D, R, K extends Array<string>> = KeysResultProcessor<D, R, K>;
+type ExistenceReporterConnector<A> = (a: A) => (i: string, b: boolean) => Task<void>;
 
-function processResultIds<D, V, K extends Array<string>>(
-  reporter: IdsReporter<D, V, K>,
-  existenceReporter: ExistenceReporter<D>,
-): KeysResultProcessor<D, Record<string, Option<V>>, K> {
-  return (driver: D, result: Record<string, Option<V>>, ...keys: K) => {
-    const entries = Object.entries(result);
-    const reporters: Array<Task<void>> = Array_.flatten(
-      entries.map(([id, maybeSubResult]) =>
-        pipe(
+export function processResultIds<A, R extends Record<string, Option<any>>>(
+  connect: ExistenceReporterConnector<A>,
+  subProcessor: SubResultProcessor<A, R>,
+): ResultProcessor<A, R> {
+  return (reporters: A, result: R, ...context: Array<string>) => {
+    const tasks: Array<Task<void>> = pipe(
+      result,
+      Record_.mapWithIndex((id, maybeSubResult) => {
+        return pipe(
           maybeSubResult,
           Option_.fold(
-            () => [existenceReporter(driver, id, false)],
-            (subResult) => [existenceReporter(driver, id, true), reporter(driver, subResult, id, ...keys)],
+            () => [connect(reporters)(id, false)],
+            (subResult) => [connect(reporters)(id, true), subProcessor(reporters, subResult, id, ...context)],
           ),
-        ),
-      ),
+        );
+      }),
+      Record_.toUnfoldable(array),
+      Array_.map(([k, v]) => v),
+      Array_.flatten,
     );
-    return Foldable_.traverse_(taskSeq, array)(reporters, identity);
+    return Foldable_.traverse_(taskSeq, array)(tasks, identity);
   };
 }
 
 // properties result contains results for a set of optional query fields
 
-export type ResultPropertyHelper<D, R, K extends Array<string>> = <B extends R[keyof R]>(
-  o: Option<B>,
-  r: (d: D, b: B, ...keys: K) => Task<void>,
-) => Array<Task<void>>;
+type ResultProcessorMapping<A, R, P extends keyof R> = Record<P, (a: A, q: R[P], ...c: Array<string>) => Task<void>>;
 
-type PropertiesResultReporter<D, R, K extends Array<string>> = (
-  r: R,
-  h: ResultPropertyHelper<D, R, K>,
-) => Array<Task<void>>;
-
-export type PropertiesResultProcessor<D, R, K extends Array<string>> = (d: D, r: R, ...k: K) => Task<void>;
-
-function processResultProperties<D, R, K extends Array<string>>(
-  reporter: PropertiesResultReporter<D, R, K>,
-): PropertiesResultProcessor<D, R, K> {
-  return (driver: D, result: R, ...keys: K) => {
-    const helper: ResultPropertyHelper<D, R, K> = (maybeSubResult, processor) =>
-      pipe(
-        maybeSubResult,
-        Option_.fold(() => [], (subResult) => [processor(driver, subResult, ...keys)]),
-      );
-    const tasks: Array<Task<void>> = reporter(result, helper);
+export function processResultProperties<A, R, P extends string & keyof R>(
+  processors: ResultProcessorMapping<A, R, P>,
+): ResultProcessor<A, R> {
+  return (reporters: A, result: R, ...context: Array<string>) => {
+    const tasks: Array<Task<void>> = pipe(
+      result,
+      Record_.mapWithIndex((property: P, subResult: R[P]) => {
+        const processor = processors[property];
+        return processor(reporters, subResult, ...context);
+      }),
+      Record_.toUnfoldable(array),
+      Array_.map(([k, v]) => v),
+    );
     return Foldable_.traverse_(taskSeq, array)(tasks, identity);
   };
 }
 
 // fields query contains information for retrieving a payload
 
-type FieldsQueryResolver<D, K extends Array<string>, R> = (d: D, ...k: K) => Task<R>;
-export type FieldsQueryProcessor<D, K extends Array<string>, R> = (d: D, q: true, ...k: K) => Task<R>;
+type FieldsQueryConnector<A, R> = (a: A) => (...k: Array<string>) => Task<R>;
 
-function processQueryFields<D, K extends Array<string>, R>(
-  resolver: FieldsQueryResolver<D, K, R>,
-): FieldsQueryProcessor<D, K, R> {
-  return (driver, query, ...keys) => resolver(driver, ...keys);
+export function processQueryFields<A, R>(connect: FieldsQueryConnector<A, R>): QueryProcessor<A, true, R> {
+  return (resolvers, query, ...context) => connect(resolvers)(...context);
 }
 
-// keys query requests some information that is always present in data source
+// keys query requests some information that is always present in database
 
-type KeysQueryResolver<D, Q extends Record<string, any>, K extends Array<string>, R> = (
-  p: D,
-  s: Q[keyof Q],
-  i: string,
-  ...k: K
-) => Task<R>;
-export type KeysQueryProcessor<D, Q, K extends Array<string>, R> = (d: D, q: Q, ...k: K) => Task<Record<string, R>>;
-
-function processQueryKeys<D, Q extends Record<string, any>, K extends Array<string>, R>(
-  resolver: KeysQueryResolver<D, Q, K, R>,
-): KeysQueryProcessor<D, Q, K, R> {
-  return (driver: D, query: Q, ...keys: K) => {
-    return pipe(
+export function processQueryKeys<A, Q extends Record<I, SQ>, I extends string, SQ, SR>(
+  subProcessor: QueryProcessor<A, SQ, SR>,
+): QueryProcessor<A, Q, Record<I, SR>> {
+  return (resolvers: A, query: Q, ...context: Array<string>) => {
+    const foo: Task<Record<I, SR>> = pipe(
       query,
-      Record_.mapWithIndex((id, subQuery: Q[keyof Q]) => resolver(driver, subQuery, id, ...keys)),
-      Apply_.sequenceS(task),
+      Record_.mapWithIndex((id: I, subQuery: SQ): Task<SR> => subProcessor(resolvers, subQuery, id, ...context)),
+      Record_.sequence(task),
     );
+    return foo;
   };
 }
 
-// keys query requests some information that may not be present in data source
+// keys query requests some information that may not be present in database
 
-type IdsQueryResolver<D, Q, K extends Array<string>, R> = KeysQueryResolver<D, Q, K, R>;
-type ExistenceCheck<D> = (d: D, i: string) => Task<boolean>;
-export type IdsQueryProcessor<D, Q, K extends Array<string>, R> = KeysQueryProcessor<D, Q, K, Option<R>>;
+type ExistenceCheckConnector<A> = (a: A) => (i: string) => Task<boolean>;
 
-function processQueryIds<D, Q extends Record<string, any>, K extends Array<string>, R>(
-  resolver: IdsQueryResolver<D, Q, K, R>,
-  existenceCheck: ExistenceCheck<D>,
-): IdsQueryProcessor<D, Q, K, R> {
-  return (driver: D, query: Q, ...keys: K) => {
-    return pipe(
+export function processQueryIds<A, Q extends Record<I, SQ>, I extends string, SQ, SR>(
+  connect: ExistenceCheckConnector<A>,
+  subProcessor: QueryProcessor<A, SQ, SR>,
+): QueryProcessor<A, Q, Record<I, Option<SR>>> {
+  return (resolvers: A, query: Q, ...context: Array<string>) => {
+    const tasks: Record<I, Task<Option<SR>>> = pipe(
       query,
-      Record_.mapWithIndex((id, subQuery: Q[keyof Q]) => {
-        return pipe(
-          existenceCheck(driver, id),
-          Task_.chain(
-            (exists): Task<Option<R>> => {
-              if (exists) {
-                return pipe(
-                  resolver(driver, subQuery, id, ...keys),
-                  Task_.map(Option_.some),
-                );
-              }
-              return Task_.of(Option_.none);
-            },
-          ),
-        );
-      }),
-      Apply_.sequenceS(task),
+      Record_.mapWithIndex(
+        (id: I, subQuery: SQ): Task<Option<SR>> => {
+          return pipe(
+            connect(resolvers)(id),
+            Task_.chain(
+              (exists): Task<Option<SR>> => {
+                if (exists) {
+                  return pipe(
+                    subProcessor(resolvers, subQuery, id, ...context),
+                    Task_.map(Option_.some),
+                  );
+                }
+                return Task_.of(Option_.none);
+              },
+            ),
+          );
+        },
+      ),
     );
+    return Record_.sequence(task)(tasks);
   };
 }
 
 // properties query contains optional queries that may or may not be present
 
-export type QueryPropertyHelper<D, Q, K extends Array<string>, R> = <
-  I extends keyof Q & keyof R,
-  A extends Q[I],
-  B extends R[I]
->(
-  p: I,
-  o: Option<A>,
-  r: (d: D, x: A, ...k: K) => Task<B>,
-) => { readonly [i in I]?: Task<B> };
+type QueryProcessorMapping<
+  A,
+  Q extends Record<string, any>,
+  R extends Record<keyof Q, any>,
+  P extends keyof Q & keyof R
+> = Record<P, (a: A, q: Q[P], ...c: Array<string>) => Task<R[P]>>;
 
-type PropertiesQueryResolver<D, Q, K extends Array<string>, R> = (
-  q: Q,
-  h: QueryPropertyHelper<D, Q, K, R>,
-) => { readonly [I in keyof R]: Task<R[I]> };
-
-export type PropertiesQueryProcessor<D, Q, K extends Array<string>, R> = (d: D, q: Q, ...k: K) => Task<R>;
-
-function processQueryProperties<D, Q, K extends Array<string>, R>(
-  resolver: PropertiesQueryResolver<D, Q, K, R>,
-): PropertiesQueryProcessor<D, Q, K, R> {
-  return (driver: D, query: Q, ...keys: K) => {
-    const helper: QueryPropertyHelper<D, Q, K, R> = (key, maybeSubQuery, processor) =>
-      pipe(
-        maybeSubQuery,
-        Option_.fold(() => ({} as any), (subQuery) => ({ [key]: processor(driver, subQuery, ...keys) })),
-      );
-    // todo: convince sequenceS to process tasks with type casts
-    const tasks: any = resolver(query, helper);
-    return (Apply_.sequenceS(task)(tasks) as unknown) as Task<R>;
+export function processQueryProperties<
+  A,
+  Q extends Record<string, any>,
+  R extends Record<keyof Q, any>,
+  P extends string & keyof Q & keyof R
+>(processors: QueryProcessorMapping<A, Q, R, P>): QueryProcessor<A, Q, Record<P, R[P]>> {
+  return (resolvers: A, query: Q, ...context: Array<string>) => {
+    const tasks: Record<P, Task<R[P]>> = pipe(
+      query,
+      Record_.mapWithIndex((property, subQuery: Q[P]) => {
+        const processor = processors[property];
+        const subResult = processor(resolvers, subQuery, ...context);
+        return subResult;
+      }),
+    );
+    return Record_.sequence(task)(tasks);
   };
 }
-
-const process = {
-  processResultFields,
-  processResultKeys,
-  processResultIds,
-  processResultProperties,
-  processQueryFields,
-  processQueryKeys,
-  processQueryIds,
-  processQueryProperties,
-};
-
-export default process;
