16dcce9874284eb63aee5e7303b0b7ab701757a1
Toni Ruottu <toni.ruottu@maas.global>
2020-02-04 18:11:16 +0200

    maybs

--- src/reduce-result.ts
+++ src/reduce-result.ts
@@ -1,16 +1,15 @@
 import { Either, either } from 'fp-ts/lib/Either';
 import { NonEmptyArray, nonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
 import * as NonEmptyArray_ from 'fp-ts/lib/NonEmptyArray';
-import { Option, None, Some, option } from 'fp-ts/lib/Option';
+import { Option, option } from 'fp-ts/lib/Option';
 import * as Either_ from 'fp-ts/lib/Either';
 import * as Array_ from 'fp-ts/lib/Array';
 import * as Option_ from 'fp-ts/lib/Option';
 import * as Record_ from 'fp-ts/lib/Record';
-import { sequenceS } from 'fp-ts/lib/Apply';
 import { Lazy } from 'fp-ts/lib/function';
 import { pipe } from 'fp-ts/lib/pipeable';
 
-import { Dict } from './dict';
+//import { Dict } from './dict';
 import * as Dict_ from './dict';
 import { mergeOption } from './option';
 
@@ -22,8 +21,8 @@ import {
   KeysResult,
   Id,
   IdsResult,
-  Terms,
-  SearchResult,
+  //Terms,
+  //SearchResult,
   Property,
   PropertiesResult,
   Err,
@@ -41,14 +40,10 @@ export const literal = <L extends LiteralResult>(
     NonEmptyArray_.tail(results),
     Array_.reduce(
       Either_.right(NonEmptyArray_.head(results)),
-      (
-        ma: Either<ReduceFailure, L>,
-        mb: Either<ReduceFailure, L>,
-      ): Either<ReduceFailure, L> =>
+      (ma: Either<ReduceFailure, L>, b: L): Either<ReduceFailure, L> =>
         pipe(
-          { a: ma, b: mb },
-          sequenceS(either),
-          Either_.chain(({ a, b }) => {
+          ma,
+          Either_.chain((a) => {
             if (JSON.stringify(a) !== JSON.stringify(b)) {
               return Either_.left(reduceeMismatch);
             }
@@ -58,17 +53,16 @@ export const literal = <L extends LiteralResult>(
     ),
   );
 
-export const leaf = <R extends LeafResult>(
-  combineLeafResult: LeafResultCombiner<R>,
-) => (results: NonEmptyArray<R>): Either<ReduceFailure, R> => {
+export const leaf = <R extends LeafResult>(combineLeafResult: LeafResultCombiner<R>) => (
+  results: NonEmptyArray<R>,
+): Either<ReduceFailure, R> => {
   const writeResult: R = NonEmptyArray_.head(results);
   const readResult: Array<R> = NonEmptyArray_.tail(results);
-
-  const foob = pipe(
+  return pipe(
     readResult,
     Array_.reduce(writeResult, combineLeafResult),
+    Either_.right,
   );
-  return foob;
 };
 
 export const keys = <K extends Key, SR extends Result>(
@@ -103,44 +97,26 @@ export const ids = <I extends Id, E extends Err, SR extends Result>(
       ): Option<Either<ReduceFailure, Either<E, Option<SR>>>> =>
         pipe(
           subResultVariants,
-          (x: NonEmptyArray<Either<E, Option<SR>>>) => x,
           nonEmptyArray.sequence(either),
-          (x: Either<E, NonEmptyArray<Option<SR>>>) => x,
-          Either_.map((foobb) =>
-            pipe(
-              foobb,
-              (x: NonEmptyArray<Option<SR>>) => x,
-              mergeOption,
-              (x: Option<NonEmptyArray<None> | NonEmptyArray<Some<SR>>>) => x,
-            ),
-          ),
-          (x: Either<E, Option<NonEmptyArray<None> | NonEmptyArray<Some<SR>>>>) => x,
+          Either_.map((foobb) => mergeOption(foobb)),
           Either_.chain(Either_.fromOption(existenceChange)),
-          (x: Either<E, NonEmptyArray<None> | NonEmptyArray<Some<SR>>>) => x,
           Either_.map(nonEmptyArray.sequence(option)),
-          (x: Either<E, Option<NonEmptyArray<SR>>>) => x,
-          Either_.map(
-            Option_.map(
-              (subResultVariants: NonEmptyArray<SR>): Either<ReduceFailure, SR> =>
-                reduceSubResult(subResultVariants),
+          Either_.map((bar) =>
+            pipe(
+              bar,
+              Option_.map((subResultVariants) => reduceSubResult(subResultVariants)),
+              option.sequence(either),
             ),
           ),
+          either.sequence(either),
           Option_.some,
-          (x: Option<Either<ReduceFailure, Either<E, Option<SR>>>>) => x,
         ),
     ),
-    (x: Option<Dict<I, Either<ReduceFailure, Either<E, Option<SR>>>>>) => x,
     Either_.fromOption(() => reduceeMismatch),
     Either_.chain(Dict_.sequenceEither),
-    (x: Either<ReduceFailure, Dict<I, Either<E, Option<SR>>>>) => x,
   );
-
-export const search = <
-  T extends Terms,
-  I extends Id,
-  E extends Err,
-  SR extends Result
->(
+/*
+export const search = <T extends Terms, I extends Id, E extends Err, SR extends Result>(
   reduceSubResult: ResultReducer<SR>,
   matchChange: Lazy<E>,
 ) => (
@@ -173,18 +149,26 @@ export const search = <
       },
     ),
   );
-
+*/
 export const properties = <R extends PropertiesResult>(
   processors: ResultReducerMapping<R>,
-) => <P extends Property & keyof R>(results: NonEmptyArray<R>): R =>
-  pipe(
+) => <P extends Property & keyof R>(
+  results: NonEmptyArray<R>,
+): Either<ReduceFailure, R> => {
+  const omg: Record<P, Either<ReduceFailure, R[P]>> = pipe(
     NonEmptyArray_.head(results),
-    Record_.mapWithIndex<P, unknown, R[P]>((propName) => {
-      const propReducer = processors[propName];
-      return pipe(
-        results,
-        NonEmptyArray_.map((r) => r[propName]),
-        propReducer,
-      );
-    }),
-  ) as R;
+    Record_.mapWithIndex<P, unknown, Either<ReduceFailure, R[P]>>(
+      (propName: P): Either<ReduceFailure, R[P]> => {
+        const propReducer: ResultReducer<R[P]> = processors[propName];
+        return pipe(
+          results,
+          NonEmptyArray_.map((r: R): R[P] => r[propName]),
+          propReducer,
+          (x: Either<ReduceFailure, R[P]>) => x,
+        );
+      },
+    ),
+  ) as Record<P, Either<ReduceFailure, R[P]>>;
+  const result: Either<ReduceFailure, Record<P, R[P]>> = Record_.sequence(either)(omg);
+  return result as Either<ReduceFailure, R>;
+};
