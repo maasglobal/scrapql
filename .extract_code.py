5d19362af2593b061717ff57c564929ce9a1c3b5
Toni Ruottu <toni.ruottu@maas.global>
2020-02-20 15:56:51 +0200

    Generate constructors from codecs

--- .extract_code.py
+++ .extract_code.py
@@ -1,3 +1,3 @@
 
 
-import sys; sys.stdout.write(('\n'.join([i.split('```')[0] for i in sys.stdin.read().split('```typescript')[1:]])).replace("from 'scrapql'", "from './scrapql'").replace("from 'scrapql/lib/", "from './"))
+import sys; sys.stdout.write(('\n'.join([i.split('```')[0] for i in sys.stdin.read().split('```typescript')[1:]])).replace("from 'scrapql'", "from './scrapql'").replace("from 'scrapql/lib/", "from './") + '\n' + 'export { Bundle, Customer, CustomerId, Errors, Json, QUERY_PROTOCOL, Query, RESULT_PROTOCOL, Report, Result, Year, client, db, example, exampleBundle, exampleJsonQuery, exampleQuery, exampleResult, exampleJsonResult, jsonQueryProcessor, packageName, packageVersion, processQuery, processResult, reporters, resolvers, server }')
